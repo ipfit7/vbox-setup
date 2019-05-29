537a99a7f94c344064d96fff672a4c83e7e3448e
Mies van der Lippe <miesvanderlippe@gmail.com>
2019-05-29 17:26:14 +0200

    Added params to create_vm script
    Script now supports vm base path and vm name.
    
    I've also added a basic check for whether the VM already exists. This is something I ran into during testing.

--- create_vm.sh
+++ create_vm.sh
@@ -1,16 +1,31 @@
-# TODO: Implement params here
-fullpath=$(pwd)
-name='KiesMondzorg'
-ostype='Debian_64'
+BASE_PATH=$(pwd)
+VM_NAME="KiesMondzorg"
 
-#TODO: ask for path to install the VM at.
+while getopts p:n:s: option
+do
+    case "${option}" in
+    p) BASE_PATH=${OPTARG};;
+    n) VM_NAME=${OPTARG};;
+    esac
+done;
+
+FULL_PATH=$BASE_PATH/$VM_NAME
+OS_TYPE='Debian_64'
+
+if [ ! -f $FULL_PATH".vbox" ]; then
+    echo "VM Already exists. Aborting..."
+    exit 1
+fi
+
 #This part creates the VM, storage controller for the VM and attaches the VDI created by create_vdi.sh
-echo "Creating VM with name " $name " at " $fullpath "..."
-vboxmanage createvm --name $name --basefolder $fullpath/KiesMondzorg --ostype $ostype --default --register
+echo "Creating VM with name " $VM_NAME " at " $BASE_PATH "..."
+vboxmanage createvm --name $VM_NAME --basefolder $BASE_PATH --ostype $OS_TYPE --default --register
+
 echo "Creating storage controller for the new VM..."
-vboxmanage storagectl $name --name mainHDD --controller IntelAhci --add sata --bootable on --portcount 1
+vboxmanage storagectl $VM_NAME --name mainHDD --controller IntelAhci --add sata --bootable on --portcount 1
+
 echo "Attaching previously generated storage medium to the VM..."
-vboxmanage storageattach $name --storagectl mainHDD --port 1 --type hdd --medium $fullpath/KiesMondzorgVDI.vdi
+vboxmanage storageattach $VM_NAME --storagectl mainHDD --port 1 --type hdd --medium $BASE_PATH/$VM_NAME"VDI.vdi"
 
 # Set some custom branding
-VBoxManage modifyvm $name --iconfile $(pwd)/res/kies-vm-icon.png
+VBoxManage modifyvm $VM_NAME --iconfile $(pwd)/res/kies-vm-icon.png
