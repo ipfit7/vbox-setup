83843d7f1346ea4568d53ccc1f242d1f0fada716
Mies van der Lippe <miesvanderlippe@gmail.com>
2019-05-29 17:04:28 +0200

    Added some checks to deleting VDI
    I'm checking if the VDI exists at all, and whether deletion was succesful.

--- create_vdi.sh
+++ create_vdi.sh
@@ -10,8 +10,19 @@ echo "Creating virtual hard disk"
 # This only deletes the VDI if it's in the same path, with the same name
 # Other HDDs with the same name may still be registered.
 # TODO: Ask user if we should delete the disk 
-# TODO: Return the UUID
-VBoxManage closemedium disk $fullPath --delete
+
+# Check if there's previous VDI's on this path
+if vboxmanage list hdds | grep $fullPath --quiet; then
+    echo "Deleting existing VDI..."
+    # Try and delete them (it's probably us anyways)
+    if VBoxManage closemedium disk $fullPath --delete  > /dev/null 2>&1; then
+        echo "Deleted VDI";
+    else
+        echo "Failed deleting VDI. Please detach it from any VM and try again."
+        exit 1;
+    fi;
+fi
+
 
 # Create the new VDI
 VBoxManage createmedium disk --filename $diskName --size $sizeMB --format VDI --variant Standard
