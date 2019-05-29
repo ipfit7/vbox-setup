fb9d6c3522a65c91d7d8d5054ee79c5dbd6906a2
Mies van der Lippe <miesvanderlippe@gmail.com>
2019-05-29 17:56:07 +0200

    Added download and check for guest additions iso
    Turns out, this isn't reliably present on all machines.

--- install_os.sh
+++ install_os.sh
@@ -10,10 +10,18 @@ do
 done;
 
 # Download debian iso if it isn't found in the current directory.
+# TODO: hash check for validity & completeness
 if [ ! -f debian.iso ]; then
     echo "Debian ISO not found... Downloading..."
     curl --output debian.iso -L https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-9.9.0-amd64-netinst.iso
-fi
+fi;
+
+# Download Guest Additions if not found. 
+# TODO: hash check for validity & completeness
+if [ ! -f guest_additions.iso ]; then
+    echo "Guest additions not found... Downloading..."
+    curl --output guest_additions.iso -L https://download.virtualbox.org/virtualbox/6.0.8/VBoxGuestAdditions_6.0.8.iso
+fi;
 
 # Configure the unattended installer
-vboxmanage unattended install $VM_NAME --iso=debian.iso --script-template=./unattended/preseed.cfg --post-install-template=./unattended/post_install.sh --install-additions --hostname=kiesmondzorg.local --start-vm=headless
+vboxmanage unattended install $VM_NAME --iso=debian.iso --additions-iso=guest_additions.iso --script-template=./unattended/preseed.cfg --post-install-template=./unattended/post_install.sh --install-additions --hostname=kiesmondzorg.local --start-vm=headless
