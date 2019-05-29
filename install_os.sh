b53a667d46e1c1ca08447ec23e38d90ce14b3725
Mies van der Lippe <miesvanderlippe@gmail.com>
2019-05-29 17:54:30 +0200

    Added params to install_os script
    Also slightly changed some of the paths. They now assume the pwd is where the repo is cloned. This will be described in installation instrucions.

--- install_os.sh
+++ install_os.sh
@@ -1,11 +1,19 @@
-fullpath=$(pwd)
-name='KiesMondzorg'
+BASE_PATH=$(pwd)
+VM_NAME="KiesMondzorg"
+
+while getopts p:n:s: option
+do
+    case "${option}" in
+    p) BASE_PATH=${OPTARG};;
+    n) VM_NAME=${OPTARG};;
+    esac
+done;
 
 # Download debian iso if it isn't found in the current directory.
-if [ ! -f $fullpath/debian.iso ]; then
+if [ ! -f debian.iso ]; then
     echo "Debian ISO not found... Downloading..."
     curl --output debian.iso -L https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-9.9.0-amd64-netinst.iso
 fi
 
 # Configure the unattended installer
-vboxmanage unattended install $name --iso=$fullpath/debian.iso --script-template=$fullpath/unattended/preseed.cfg --post-install-template=$fullpath/unattended/post_install.sh --install-additions --hostname=kiesmondzorg.local --start-vm=headless
+vboxmanage unattended install $VM_NAME --iso=debian.iso --script-template=./unattended/preseed.cfg --post-install-template=./unattended/post_install.sh --install-additions --hostname=kiesmondzorg.local --start-vm=headless
